126a2df73b61551b91cc0a94f7171c849e53c27a
Portugapt <monteiro.joao.ps@gmail.com>
2024-12-05 21:26:40 +0000

    feat: Generate calls posts

--- scripts/generate_site.py
+++ scripts/generate_site.py
@@ -1,11 +1,20 @@
-import electric_toolbox
-from jinja2 import Environment, PackageLoader
+"""Script to generate website."""
+
 from pathlib import Path
 
-WEBSITE_DIRECTORY: Path = Path("website")
+from jinja2 import Environment, PackageLoader
+
+import electric_toolbox
+
+WEBSITE_DIRECTORY: Path = Path('website')
 electric_toolbox.clean_and_recreate(WEBSITE_DIRECTORY)
 
 
-jinja_env = Environment( loader= PackageLoader('electric_toolbox', 'templates'))
+jinja_env = Environment(
+    loader=PackageLoader('electric_toolbox', 'templates'),
+    autoescape=True,
+)
 
 electric_toolbox.build_index(path=WEBSITE_DIRECTORY, j2_env=jinja_env)
+
+electric_toolbox.read_posts(path=Path('content'), folder='posts')
