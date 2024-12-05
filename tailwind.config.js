e56fdadf370faee547bf3b8dbfbf507024769101
Portugapt <monteiro.joao.ps@gmail.com>
2024-12-05 00:40:27 +0000

    slight changes to configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./eletric_toolbox/templates/**/*.{html,js,j2}"],
+  content: ["src/electric_toolbox/templates/**/*.{html,js,j2}"],
   theme: {
     extend: {},
   },
