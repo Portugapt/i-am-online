045b1bb1258801b8851e2539efadb23ad315ef4b
Portugapt <monteiro.joao.ps@gmail.com>
2024-12-05 00:03:16 +0000

    add tailwind resources

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./eletric_toolbox/templates/**/*.{html,js,j2}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
