01bcb6d97c86aad1f1bc734ac0d0ee618c3ff626
Portugapt <monteiro.joao.ps@gmail.com>
2024-12-05 00:40:57 +0000

    cleanups

--- src/electric_toolbox/index.py
+++ src/electric_toolbox/index.py
@@ -4,7 +4,7 @@ from jinja2 import Environment
     
 
 def build(path: str, j2_env: Environment,) -> None:
-    nav = j2_env.get_template("index/navbar.j2")
+    nav = j2_env.get_template("index/index.j2")
     contents = nav.render()
 
     to_file(
