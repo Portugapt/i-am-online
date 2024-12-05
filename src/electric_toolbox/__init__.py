a37ab7b348059f45e9d4ac4ef5e72f688b476d68
Portugapt <monteiro.joao.ps@gmail.com>
2024-12-05 21:10:33 +0000

    feat: Add a listing of files function

--- src/electric_toolbox/__init__.py
+++ src/electric_toolbox/__init__.py
@@ -1,2 +1,11 @@
-from .index import build as build_index # noqa: F401
-from .common.folder_management import clean_and_recreate
+"""Electric Toolbox."""
+
+from .common.files_management import clean_and_recreate
+from .index import build as build_index
+from .posts import read_posts
+
+__all__ = [
+    'build_index',
+    'clean_and_recreate',
+    'read_posts',
+]
